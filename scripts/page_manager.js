eeb171d0d3f7de518d0405ff43f713bebc305e8e
tycelm <70135829+tycelm@users.noreply.github.com>
2023-02-21 13:04:16 -0500

    thats all the projects

--- scripts/page_manager.js
+++ scripts/page_manager.js
@@ -1,3 +1,19 @@
 function change_page(name){
     window.location.href = name;
 }
+
+// collapsible manager
+var collapsible = document.getElementsByClassName("scrapped");
+var i;
+
+for(i = 0; i < collapsible.length; i++){
+    collapsible[i].addEventListener("click", () => {
+        var scrapped_content = document.getElementsByClassName("scrapped_content")[0];
+        if (scrapped_content.style.maxHeight){
+            scrapped_content.style.maxHeight = null;
+        }
+        else{
+            scrapped_content.style.maxHeight = scrapped_content.scrollHeight + "px";
+        }
+     });
+}
