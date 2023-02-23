3e6a894ac9a4b529c1ce0561de27ed26497a6730
tycelm <70135829+tycelm@users.noreply.github.com>
2023-02-23 15:46:47 -0500

    finished everything on the left!

--- scripts/page_manager.js
+++ scripts/page_manager.js
@@ -17,3 +17,23 @@ for(i = 0; i < collapsible.length; i++){
         }
      });
 }
+
+//used to return
+function back(){
+    document.getElementById('top').scrollIntoView();
+    document.getElementById("anim").src = "";
+    document.getElementById("anim").src = "assets/prog_out.gif";
+
+    var remove = document.getElementsByClassName("remove");
+    var j;
+
+    for(j = remove.length - 1; j >=0; j--){
+        remove[j].remove();
+    }
+
+    var delayInMilliseconds = 4000; // 4 seconds
+
+    setTimeout(function() {
+        window.location.href = 'index.html'
+    }, delayInMilliseconds);
+}
